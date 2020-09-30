3201489a1cf7453ee59d29ff47fe741bccb4c33e
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-30 13:01:36 +0000

    logout added

--- express_server.js
+++ express_server.js
@@ -30,6 +30,10 @@ app.post('/login', (req, res) => {
   }
   res.redirect('/urls');
 });
+app.post('/logout', (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls');
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
