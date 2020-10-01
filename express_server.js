913ce933ab436607ad289601a5ab6aff0ebabbe4
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-10-01 20:25:30 +0000

    refactored

--- express_server.js
+++ express_server.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
+
+// hashs the password
 const bcrypt = require('bcrypt');
+
+// encrypts the cookies
 const cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: 'session',
   keys: ['abacaxi', 'melancia'],  
 }));
 
-const { getUserByEmail, generateRandomString, urlsForUser } = require('./helpers')
+// access to helper functions
+const { getUserByEmail, generateRandomString, urlsForUser } = require('./helpers');
+
+// access to databases
 const users = require('./databases');
 const urlDatabase = require('./databases');
 
@@ -25,16 +34,7 @@ app.get("/urls.json", (req, res) => {
 
 
 
-// -->> ALL GET ROUTES:
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");	 
-  urlDatabase[shortURL] = {
-    longURL: req.body.longURL,
-    userID: req.cookies['user_id']
-  }
-  res.redirect(`/urls/${shortURL}`); 
-});
+// ---- >> ALL GET ROUTES << ----
 
 // GET -> URLs, pass the URL data to our template.
 app.get('/urls', (req, res) => {
@@ -55,9 +55,6 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => { 
   if (urlDatabase[req.params.shortURL].userID !== req.session.user_id) {
     res.send("You are not authorized");
-    console.log(urlDatabase)
-  console.log(req.session.user_id)
-  console.log(urlDatabase[req.params.shortURL])
     return;
   }
   let templateVars = {
@@ -87,7 +84,7 @@ app.get('/register', (req, res) => {
 });
 
 
-// -->> ALL POST ROUTES:
+// --- >> ALL POST ROUTES << ---
 
 // POST-> URLS
 app.post("/urls", (req, res) => {
@@ -104,6 +101,7 @@ app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
   const userID = req.session.user_id;
+  //
   if (longURL.match(/^(https:\/\/|http:\/\/)/)) {
     urlDatabase[shortURL] = {
       longURL,
@@ -121,7 +119,12 @@ app.post('/urls/:shortURL', (req, res) => {
 // POST -> removes a URL resource: POST /urls/:shortURL/delete
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
+  const userID = req.session.user_id;
+  const user = users[userID];
+  const urls = urlsForUser(user, urlDatabase);
+  if (Object.keys(urls).includes(shortURL)) {
   delete urlDatabase[shortURL];
+  }
   res.redirect("/urls");
 });
 
@@ -129,15 +132,18 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  // returns an error if the user do not provide email and password
   if (!email || !password) {
     return res.status(400).send("<h1>400 Bad Request</h1><p>Please fill up all fields.</p>");
   } else {
     const user = getUserByEmail(email, users);
     // compare password with hash password value
-    const hashss = bcrypt.compareSync(password, user.password); 
+    const hashss = bcrypt.compareSync(password, user.password);
+    // user redirected to urls page and has access to create new URL and their own URL library 
     if (hashss) {
       req.session.user_id = user.id
       res.redirect('/urls');
+      // returns an error if email/password doesnt match
     } else {
       res.status(403).send("<h1>Email or Password is not correct</h>");
     }
@@ -147,20 +153,22 @@ app.post('/login', (req, res) => {
 // POST -> register endpoint, which returns the template register
 app.post('/register', (req, res) => {
   const id = generateRandomString();
+  // extract the info from the form
   const { email, password } = req.body;
-  
+  // check if all fields are filled
   if (email === "" || password === '') {
     return res.status(400).send("<h1>400 Bad Request</h1><p>Please fill up all fields.</p>");
   }
+  // check if the user is not already in the database
     if (getUserByEmail(email, users)) {
       return res.status(400).send("<h1>400 Bad Request</h1><p>User is already registered. Please, make sure you are registering a new user.</p>");
     }
+    // if user doesnt exists yet, it'ok to add the user to the database
   users[id] = {
     id,
     email,
     password: bcrypt.hashSync(password, 10)
   };
-  console.log(users)
   req.session.user_id = id;
   return res.redirect('/urls');
 });
@@ -168,6 +176,7 @@ app.post('/register', (req, res) => {
 
 // POST -> logout
 app.post('/logout', (req, res) => {
+  // clear the cookies
   res.clearCookie("session");
   res.clearCookie("session.sig");
   res.redirect('/login');
@@ -176,6 +185,6 @@ app.post('/logout', (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
 
